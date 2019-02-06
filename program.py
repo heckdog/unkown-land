70cffc1e94e447c6bc49ea8e03cee2a31e2015a7
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-02-06 08:24:23 -0500

    update version number

--- program.py
+++ program.py
@@ -16,7 +16,7 @@ import world
 # naming convention as follows:
 # RELEASE.BIGUPDATE.Run (BUILD)
 build = data.load_version()
-print("Version 0.6.4 (Build {})".format(build))
+print("Version 0.7.0 (Build {})".format(build))
 
 # uncomment this during development to increase build number. comment for full release
 data.save_version(build)
