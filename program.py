c4c83c9a8684e7a53f4db7d80b6a9612195b17b6
DevHub <neibenne21@go.camden.k12.nc.us>
2019-02-18 12:17:11 -0500

    disabled dev mode
    ready for release

--- program.py
+++ program.py
@@ -16,10 +16,10 @@ import world
 # naming convention as follows:
 # RELEASE.BIGUPDATE.Run (BUILD)
 build = data.load_version()
-print("Version 0.7.0 (Build {})".format(build))
+print("Version 0.7.1 (Build {})".format(build))
 
 # uncomment this during development to increase build number. comment for full release
-data.save_version(build)
+# data.save_version(build)
 
 """
 def test():
